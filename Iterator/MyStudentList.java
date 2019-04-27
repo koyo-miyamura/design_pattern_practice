4d3b49e75bc35536283eab859f840e1cdd868f44
koyo <nexus9071@gmail.com>
2019-04-27 18:43:07 +0900

    イテレータパターン

---
+++ Iterator/MyStudentList.java
@@ -0,0 +1,13 @@
+public class MyStudentList extends StudentList implements Aggregate{
+
+  public MyStudentList(){
+    super();
+  }
+  public MyStudentList(int studentCount){
+      super(studentCount);
+  }
+
+  public Iterator iterator(){
+      return new MyStudentListIterator(this);
+  }
+}
